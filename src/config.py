2a17761c90c601d35e0d5dad02c1f2ae03794f1a
Rayan Fakhreddine <rmf40@mail.aub.edu>
2024-11-19 20:11:36 +0200

    Add configuration and authentication setup for GPA Calculator API

---
+++ src/config.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    class APP:
+        TITLE = "GPA Calculator"
+        DESCRIPTION = "A simple GPA calculator API"
+        VERSION = "0.1.0"
+
+    class SUPABASE:
+        KEY = os.getenv("SUPABASE_KEY")
+        URL = os.getenv("SUPABASE_URL")
+
+    class JWT:
+        SECRET = os.getenv("JWT_SECRET")
+        ALGORITHM = "HS256"
+        AUDIENCE = "authenticated"
+
+    class Testing:
+        class RANDOM:
+            SEED = 0
---
+++ src/dependencies.py
@@ -0,0 +1,36 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.api_key import APIKeyHeader
+from jwt import InvalidTokenError, decode
+
+from src.config import Config
+
+
+def decode_jwt(token: str) -> dict[str, str]:
+    if Config.JWT.SECRET is None:
+        raise ValueError("JWT_SECRET must be set in the environment")
+    decoded_data: dict[str, str] = decode(
+        token,
+        key=Config.JWT.SECRET,
+        algorithms=[Config.JWT.ALGORITHM],
+        audience=Config.JWT.AUDIENCE,
+    )
+    return decoded_data
+
+
+async def get_access_token(
+    bearer: HTTPAuthorizationCredentials = Depends(HTTPBearer(scheme_name="Bearer")),
+) -> str:
+    try:
+        decode_jwt(bearer.credentials)
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid token: {e}"
+        )
+    return bearer.credentials
+
+
+async def get_refresh_token(
+    token: str = Depends(APIKeyHeader(name="refresh-token")),
+) -> str:
+    return token
