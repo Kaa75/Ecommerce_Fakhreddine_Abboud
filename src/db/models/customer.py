b290859ba86d6aa021052da5183a3c8f6779a50f
Rayan Fakhreddine <rmf40@mail.aub.edu>
2024-11-20 16:05:52 +0200

    Add validation method for Supabase user in Customer model

--- src/db/models/customer.py
+++ src/db/models/customer.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from pydantic import EmailStr, PositiveFloat, PositiveInt
+from gotrue.types import User as GoTrueUser
 
 from src.db.models import BaseModel
 from src.utils.types.UuidStr import UuidStr
@@ -16,3 +17,13 @@ class Customer(BaseModel):
     address: str
     marital_status: str
     wallet: PositiveFloat = 0.0
+
+    @classmethod
+    def validate_supabase_user(cls, customer: GoTrueUser) -> "Customer":
+        return cls(
+            id=customer.id,
+            email=customer.email,
+            fullname=customer.user_metadata["first last"],
+            username=customer.user_metadata["username"],
+            age=customer.user_metadata["age"],
+        )
